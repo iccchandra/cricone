4e6b006d61e12be4ac0e519b3013a361a10a2f0e
Ravikanth <raviknth40@gmail.com>
2020-10-06 17:22:36 +0530

    Removed Registration screen and made it as OTP.

--- app/src/main/java/com/onecricket/activity/LoginActivity.java
+++ app/src/main/java/com/onecricket/activity/LoginActivity.java
@@ -106,102 +106,61 @@ public class LoginActivity extends AppCompatActivity implements ResponseManager,
         loginPreferences = getSharedPreferences("loginPrefs", MODE_PRIVATE);
         loginPrefsEditor = loginPreferences.edit();
         saveLogin = loginPreferences.getBoolean("saveLogin", false);
-        binding.tvForgotPassword.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        binding.tvForgotPassword.setOnClickListener(view -> {
 
-                EmailorMobile = binding.etEmailMobile.getText().toString().trim();
+            EmailorMobile = binding.etEmailMobile.getText().toString().trim();
 
-                if (EmailorMobile.equals("")){
-                    ShowToast(context,"Enter Email or Mobile");
-                }else if (EmailorMobile.contains("@")) {
-                    if (!Validations.isValidEmail(EmailorMobile)) {
-                        binding.etEmailMobile.requestFocus();
-                        ShowToast(context, "Enter Valid Email Id");
-                    } else {
-                        Back = "1";
+            if (EmailorMobile.equals("")){
+                ShowToast(context,"Enter Mobile Number");
+            }else if (EmailorMobile.contains("@")) {
+                if (!Validations.isValidEmail(EmailorMobile)) {
+                    binding.etEmailMobile.requestFocus();
+                    ShowToast(context, "Enter Valid Email Id");
+                } else {
+                    Back = "1";
 
-                    }
-                } else if (TextUtils.isDigitsOnly(EmailorMobile)) {
-                    if (!EmailorMobile.matches(Validations.MobilePattern)) {
-                        binding.etEmailMobile.requestFocus();
-                        ShowToast(context, "Enter Valid Mobile Number");
-                    }
-                    else {
-                        Back = "1";
-
-                    }
+                }
+            } else if (TextUtils.isDigitsOnly(EmailorMobile)) {
+                if (!EmailorMobile.matches(Validations.MobilePattern)) {
+                    binding.etEmailMobile.requestFocus();
+                    ShowToast(context, "Enter Valid Mobile Number");
                 }
                 else {
-                    binding.etEmailMobile.requestFocus();
-                    ShowToast(context, "Enter Valid Mobile Number or Email");
+                    Back = "1";
+
                 }
+            }
+            else {
+                binding.etEmailMobile.requestFocus();
+                ShowToast(context, "Enter Valid Mobile Number");
             }
         });
 
 
-        binding.tvLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                EmailorMobile = binding.etEmailMobile.getText().toString();
-                Password = binding.etPassword.getText().toString();
-                if (EmailorMobile.equals("")){
-                    ShowToast(context,"Enter Email or Mobile");
-                }
-                else
-                if (Password.equals("")){
-
-                    ShowToast(context,"Enter Password");
-                } else if (Password.length() < 8 && !Validations.isValidPassword(Password)) {
-
-                    ShowToast(context,"Password Pattern Not Macthed");
-                }
-                else {
-                    SLoginType = "Normal";
-                    callLoginApi(true);
-
-                }
+        binding.tvLogin.setOnClickListener(view -> {
+            onForgotPasswordClicked();
+/*            EmailorMobile = binding.etEmailMobile.getText().toString();
+            Password = binding.etPassword.getText().toString();
+            if (EmailorMobile.equals("")){
+                ShowToast(context,"Enter Email or Mobile");
             }
+            else
+            if (Password.equals("")){
+
+                ShowToast(context,"Enter Password");
+            } else if (Password.length() < 8 && !Validations.isValidPassword(Password)) {
+
+                ShowToast(context,"Password Pattern Not Macthed");
+            }
+            else {
+                SLoginType = "Normal";
+                callLoginApi(true);
+
+            }*/
         });
 
-        binding.tvForgotPassword.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                EmailorMobile = binding.etEmailMobile.getText().toString();
-
-                if (EmailorMobile.equals("")){
-                    ShowToast(context,"Enter Email or Mobile");
-                }else if (EmailorMobile.contains("@")) {
-                    if (!Validations.isValidEmail(EmailorMobile)) {
-                        binding.etEmailMobile.requestFocus();
-                        ShowToast(context, "Enter Valid Email Id");
-                    } else {
-                        Intent i = new Intent(activity, ForgotVerifyOTPActivity.class);
-                        i.putExtra("type","Email");
-                        i.putExtra("EmailorMobile",EmailorMobile);
-                        startActivity(i);
-
-                    }
-                } else if (TextUtils.isDigitsOnly(EmailorMobile)) {
-                    if (!EmailorMobile.matches(Validations.MobilePattern)) {
-                        binding.etEmailMobile.requestFocus();
-                        ShowToast(context, "Enter Valid Mobile Number");
-                    }
-                    else {
-                        Intent i = new Intent(activity, ForgotVerifyOTPActivity.class);
-                        i.putExtra("type","Number");
-                        i.putExtra("EmailorMobile",EmailorMobile);
-                        startActivity(i);
-
-                    }
-                }
-                else {
-                    binding.etEmailMobile.requestFocus();
-                    ShowToast(context, "Enter Valid Mobile Number or Email");
-                }
-
-
-            }
+        binding.tvForgotPassword.setOnClickListener(view -> {
+            onForgotPasswordClicked();
         });
 
         binding.tvSignUpText.setOnClickListener(view -> {
@@ -273,6 +232,40 @@ public class LoginActivity extends AppCompatActivity implements ResponseManager,
             }
         });
 
+    }
+
+    private void onForgotPasswordClicked() {
+        EmailorMobile = binding.etEmailMobile.getText().toString();
+
+        if (EmailorMobile.equals("")){
+            ShowToast(context,"Enter Mobile Number");
+        }else if (EmailorMobile.contains("@")) {
+            if (!Validations.isValidEmail(EmailorMobile)) {
+                binding.etEmailMobile.requestFocus();
+                ShowToast(context, "Enter Valid Email Id");
+            } else {
+                Intent i = new Intent(activity, ForgotVerifyOTPActivity.class);
+                i.putExtra("type","Email");
+                i.putExtra("EmailorMobile",EmailorMobile);
+                startActivity(i);
+
+            }
+        } else if (TextUtils.isDigitsOnly(EmailorMobile)) {
+            if (!EmailorMobile.matches(Validations.MobilePattern)) {
+                binding.etEmailMobile.requestFocus();
+                ShowToast(context, "Enter Valid Mobile Number");
+            }
+            else {
+                Intent i = new Intent(activity, ForgotVerifyOTPActivity.class);
+                i.putExtra("type","Number");
+                i.putExtra("EmailorMobile",EmailorMobile);
+                startActivity(i);
+            }
+        }
+        else {
+            binding.etEmailMobile.requestFocus();
+            ShowToast(context, "Enter Valid Mobile Number");
+        }
     }
 
     private void callLoginApi(boolean isShowLoader) {

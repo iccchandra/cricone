f646c3e3361848baea88016a24fbe08dd7fc876f
ravi <iccchandra@gmail.com>
2020-11-16 14:29:04 +0530

    AI-4.0.1 <kravichandra@localhost Overwrite remote https://github.com/iccchandra/cricone.git to local

--- app/src/main/java/com/game/onecricket/adapter/Predictions.java
+++
@@ -1,7 +0,0 @@
-package com.game.onecricket.adapter;
-
-public enum Predictions {
-    UPCOMING,
-    INPROGRESS,
-    FINISHED
-}
--- app/src/main/java/com/game/onecricket/location/AppConstants.java
+++
@@ -1,7 +0,0 @@
-package com.game.onecricket.location;
-
-public class AppConstants {
-
-    public static final int LOCATION_REQUEST = 1000;
-    public static final int GPS_REQUEST = 1001;
-}
